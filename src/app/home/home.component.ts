2854ae65b2ca308708aac4216cdb8869dc773e50
L-millerd <lisa.millerd@gmail.com>
2022-08-03 10:56:29 -0700

    rebuild gsap correction

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -35,7 +35,7 @@ export class HomeComponent implements OnInit {
 
 
   ngOnInit(): void {
-    gsap.registerPlugin(ScrollTrigger, Draggable);
+    gsap.registerPlugin(ScrollTrigger);
     //hero section enter
     gsap.from('.heroContent', {yPercent: -20, opacity: 0, duration: 2, ease: "power2.inOut"});
 
